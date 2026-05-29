7ff78c0adfb878f64d79bae037786becf5e9340c
2610718-ui <2610718@sh.re.kr>
2026-05-29 02:43:50 +0000

    10718

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🔢 구구단 전체 출력")
+
+for i in range(2, 10):
+    st.subheader(f"--- {i}단 ---")
+    for j in range(1, 10):
+        st.text(f'{i} * {j} = {i*j}')
